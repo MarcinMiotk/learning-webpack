ddd613be863b7465ed2bc0a4f752a155e93abc75
Marcin Miotk <mami@snmill.com>
2017-11-13 16:05:34 +0100

    Loading data to page from JQuery

--- activeTable/src/main.ts
+++ activeTable/src/main.ts
@@ -1,7 +1,31 @@
-console.log("Hello World");
-console.log("I am main TypeScript");
-
+import {Lead, LeadProvider, LeadProviderFake} from "./Lead";
+import {LeadProviderAjax} from "./LeadProviderAjax";
+import {Users, UsersProvider, UsersProviderFake} from "./User";
+import {UsersProviderAjax} from "./UsersProviderAjax";
+import {UIReportBuilder} from "./UIReportBuilder";
 
 jQuery(function () {
-    $("div.comment").text("This text was included from JQuery");
+
+    let fake:boolean = false;
+
+
+    let leadProvider:LeadProvider;
+    let usersProvider:UsersProvider;
+
+    if(fake) {
+        leadProvider = new LeadProviderFake();
+        usersProvider = new UsersProviderFake();
+    } else {
+        leadProvider = new LeadProviderAjax();
+        usersProvider = new UsersProviderAjax();
+    }
+
+    leadProvider.get((lead:Lead)=> {
+        $("p.lead").text(lead.name);
+    });
+
+    usersProvider.get((users:Users)=> {
+        new UIReportBuilder("table.table tbody").apply(users);
+    });
+
 })
